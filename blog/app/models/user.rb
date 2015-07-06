b9a9abaf1a7abdee8e2d3340d650ac80c3f7e8d4
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-06 06:02:45 -0500

    Add User model validations for email and user_name

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]\.[a-z]+\z/i # NEEDS FIXING -- currently allows person@email..com
+  validates :email, presence: true, length: { maximum: 225 }, format: { with: VALID_EMAIL_REGEX }
+  validates :user_name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
 end
