8beae572bdbac4c3d2e6630b410533cf82366c72
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-06 06:10:12 -0500

    Add reference to secure password in User model; generate password_hash migration

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]\.[a-z]+\z/i # NEEDS FIXING -- currently allows person@email..com
   validates :email, presence: true, length: { maximum: 225 }, format: { with: VALID_EMAIL_REGEX }
   validates :user_name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706105616) do
+ActiveRecord::Schema.define(version: 20150706110747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20150706105616) do
     t.string   "last_name"
     t.string   "user_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_foreign_key "comments", "articles"
